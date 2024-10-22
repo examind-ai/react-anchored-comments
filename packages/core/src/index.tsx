1a835ed533eab6f34b855049cd6404027294ca42
Johnny Oshika <johnnyoshika@gmail.com>
2024-10-22 16:37:55 -0700

    Add core's index.tsx

---
+++ packages/core/src/index.tsx
@@ -0,0 +1,15 @@
+// Components
+export { default as CommentableContainer } from './components/CommentableContainer';
+export { default as CommentableSection } from './components/CommentableSection';
+export { default as CommentPosition } from './components/CommentPosition';
+export { default as CommentsSection } from './components/CommentsSection';
+export { default as Highlight } from './components/Highlight';
+export { default as NewComment } from './components/NewComment';
+export { default as NewCommentTrigger } from './components/NewCommentTrigger';
+
+// Contexts & their hooks
+export { createCommentsContext } from './contexts/createCommentsContext';
+export { SelectionProvider } from './contexts/SelectionContext';
+
+// Types
+export type { Comment, SelectionRange } from './types';
